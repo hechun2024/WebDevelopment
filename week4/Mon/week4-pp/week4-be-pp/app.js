ee1356d6d7ab48bbde3a070bd0b07f127c9a4a4c
hechun2024 <Chun.He@metropolia.fi>
2025-11-12 00:39:01 +0200

    Step 5: Implement Third-Party Middleware

--- week4/Mon/week4-pp/week4-be-pp/app.js
+++ week4/Mon/week4-pp/week4-be-pp/app.js
@@ -2,13 +2,16 @@ const express = require("express");
 const app = express();
 const tourRouter = require("./routes/tourRouter");
 const userRouter = require("./routes/userRouter");
+const morgan = require("morgan");
+// 使用 morgan 记录请求日志
+app.use(morgan("tiny"));
 
 // Middleware to parse JSON — 必须早于路由挂载
 app.use(express.json());
 
 // 挂载 routers
-app.use("/users", userRouter);
-app.use("/tours", tourRouter);
+app.use("/api/tours", tourRouter);
+app.use("/api/users", userRouter);
 
 const port = 4000;
 // Start the server
